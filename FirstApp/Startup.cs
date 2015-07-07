89a63ccc33fda4ebd1c3d29125384948afd41779
Austin <austinjroeder@gmail.com>
2015-07-07 09:42:18 -0700

    Built Project

---
+++ FirstApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstApp.Startup))]
+namespace FirstApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
